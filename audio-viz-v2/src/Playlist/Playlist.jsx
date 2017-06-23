9af2984563fdda34df15f28635e552fc3e4579c0
misspia <pia_leung@hotmail.com>
2017-06-22 23:00:04 -0400

    store pause/play/stop states

--- audio-viz-v2/src/Playlist/Playlist.jsx
+++ audio-viz-v2/src/Playlist/Playlist.jsx
@@ -28,36 +28,51 @@
 // export default Playlist;
 
 import React, { Component } from 'react';
-
+import Track from './Track/Track.jsx';
 import './Playlist.scss';
 
 
 class Playlist extends Component {
 	constructor() {
 		super();
-		this.state = { tracks: [] }
+		this.state = { tracks: [], audioSrc: "" }
 	}
 	populateList(files) {
 		let audio = files.map( (file, index) => { 
-			// console.log(file);
+			return <Track key={index} name={file.name} src={file.url} paused={file.paused} index={index}/>
 			// new Track components will not play any sound but instead
 			// will have audio controls that will be interpreted in Playlist
 		})
 		return audio;
 	}
 	componentWillReceiveProps(nextProps) {
+		console.log(nextProps);
+
 		this.setState({ tracks: this.populateList(nextProps.files)});
+		this.getAudioSrc(nextProps.files);
 		// update central audio tag src
 		// only 1 audio tag as only 1 song will be playing at a time
 		// if already playing: pause
 		// if not already playing: stop
 	}
+	getAudioSrc(files) {
+		this.setState({
+			audioSrc: files.forEach((file, index)=>{ if(!file.paused) return file.url; })
+		}) 
+	}
 	render() {
 		// console.log(this.state.tracks);
-		return <ul> {this.state.tracks} </ul>;
+		return (
+			<ul> 
+				<audio src={this.state.audioSrc} autoPlay controls/>
+				{this.state.tracks}
+			</ul>
+		);
+			
 	}
 }
 
 export default Playlist;
 
+// {this.state.tracks}
 
--- audio-viz-v2/src/store.js
+++ audio-viz-v2/src/store.js
@@ -8,16 +8,36 @@ const Store = flux.createStore({
 	ADD_FILE: (files) => {
 		for(let key in files) { 
 			if(typeof files[key] === 'object') { 
-				State.files.push( { name: files[key].name, url: URL.createObjectURL(files[key]), paused: true} )
+				State.files.push({ 
+					name: files[key].name,
+					url: URL.createObjectURL(files[key]),
+					playing: false,
+					selected: false
+				})
 			}	
 		};
 	},
-	PLAY_TRACK: (playIndex) => {
+	// PLAY_TRACK: (trackIndex) => {
+	// 	for (let i = 0; i < State.files.length; i ++) {
+	// 		if(trackIndex === i) {
+	// 			State.files[i].paused = false;
+	// 		} else {
+	// 			State.files[i].paused = true;
+	// 		}
+	// 	}
+	// }
+	PLAY_TRACK: (trackIndex) => {
 		for (let i = 0; i < State.files.length; i ++) {
-			if(playIndex === i) {
-				State.files[i].paused = false;
-			} else {
-				State.files[i].paused = true;
+
+			let file = State.files[i];
+			if(trackIndex === i && file.playing == true && file.selected == true) { // pause
+				file.playing = false;
+			} else if(trackIndex === i) { // play
+				file.playing = true;
+				file.playing = true;
+			} else { // stop
+				file.playing = false;
+				file.selected = false;
 			}
 		}
 	}
