73a3eaf4f5af69a3b17c13f6dc36e74eaacb0900
misspia <pia_leung@hotmail.com>
2017-07-08 22:03:13 -0400

    set up canvas context & test build

--- src/Visualizer/Visualizer.jsx
+++ src/Visualizer/Visualizer.jsx
@@ -4,21 +4,38 @@ import './Visualizer.scss';
 class Visualizer extends Component {
 	constructor() {
 		super();
-		this.state = { frequencyData: []}
+		this.state = { GL: null, frequencyData: []}
+	}
+	componentDidMount() {
+		this.resize();
+		this.setState({ GL: this.refs.canvas.getContext('webgl', {antialias: false}) });
+		// this.setState({ GL: this.refs.canvas.getContext('2d') });
+
 	}
 	componentWillReceiveProps(nextProps){
 		// console.log(nextProps.frequencyByte);
 		if(nextProps.frequencyByte === undefined) return;
-		this.state.frequencyData.push(nextProps.frequencyByte)
+		this.state.frequencyData.push(nextProps.frequencyByte);
+
+	}
+	resize() {
+		this.refs.canvas.width = this.refs.container.clientWidth;
+		this.refs.canvas.height = this.refs.container.clientHeight;
 	}
 	render() {
-		// console.log(this.state.frequencyData);
-		// return <li id="visualizer" ref="canvas"></li>
-		return <li id="visualizer" className="row">
-			<p>i is here</p>
-			<p>{this.props.frequencyByte} </p>
-			<p>{this.state.frequencyData}</p>
-		</li>
+		if(this.state.GL) {
+			console.log(this.state.GL);
+			// this.state.GL.beginPath();
+			// this.state.GL.moveTo(0,0);
+			// this.state.GL.lineTo(300,150);
+			// this.state.GL.stroke();
+		}
+		
+
+		return <div ref="container" className="canvas_container">
+			<canvas ref="canvas"></canvas>
+		</div>;
+		
 	}
 
 };
