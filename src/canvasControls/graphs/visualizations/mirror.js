f3b40c88424fedc9c340311a920d5c19e9e1fbab
misspia <pia_leung@hotmail.com>
2017-08-12 19:53:38 -0400

    bars and reflected line graph

--- src/canvasControls/graphs/visualizations/mirror.js
+++ src/canvasControls/graphs/visualizations/mirror.js
@@ -1,6 +1,6 @@
 import Utils from '../graphs.utils.js';
 
-function Line( ctx, begin={}, end={}, color, lineWidth = '2') {
+function Line( ctx, begin={}, end={}, color, lineWidth = '2', lineCap='butt') {
 	this.begin = begin;
 	this.end = end;
 
@@ -8,6 +8,7 @@ function Line( ctx, begin={}, end={}, color, lineWidth = '2') {
 		ctx.beginPath();
 		ctx.lineWidth = lineWidth;
 		ctx.strokeStyle = color;
+		ctx.lineCap = lineCap;
 		ctx.moveTo(this.begin.x , this.begin.y);
 		ctx.lineTo(this.end.x, this.end.y);
 		ctx.stroke();
@@ -16,11 +17,12 @@ function Line( ctx, begin={}, end={}, color, lineWidth = '2') {
 
 function animate(canvas, ctx, analyser, colorGenerator) {
 	if(!analyser.frequencyBinCount) return;
-
-	const frequencyData = new Uint8Array(150);
 	
-	function renderBars(barWidth, maxHeight, node, index, color) {
-		const x = index * (1 + barWidth);
+	analyser.fftSize = 256;
+	const frequencyData = new Uint8Array(90);
+	
+	function renderBars(xSpread, barWidth, maxHeight, node, index, color) {
+		const x = index * (1 + xSpread);
 		const barHeight = Utils.upTo(maxHeight, Utils.maxNode, node);
 		const begin = {
 			x: x,
@@ -30,11 +32,15 @@ function animate(canvas, ctx, analyser, colorGenerator) {
 			x: x,
 			y: begin.y - barHeight
 		};
-		const bar = new Line(ctx, begin, end, color, barWidth);
+		const bar = new Line(ctx, begin, end, color, barWidth, 'round');
 		bar.draw();
 	}
-	function renderLine() {
+	function renderLine(xStart, xEnd, maxHeight, node, nextNode, color) {
+		const begin = { x: xStart, y: canvas.height / 2 + Utils.upTo(maxHeight, Utils.maxNode, node) };
+		const end = { x: xEnd, y: canvas.height / 2 + Utils.upTo(maxHeight, Utils.maxNode, nextNode) };
 
+		const line = new Line(ctx, begin, end, color);
+		line.draw();
 	}
 	function renderMirror() {
 		requestAnimationFrame(renderMirror);
@@ -43,12 +49,20 @@ function animate(canvas, ctx, analyser, colorGenerator) {
 
 		ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-		const barWidth = canvas.width / frequencyData.length;
-		const maxHeight = canvas.height * 0.4;
+		const xSpread = canvas.width / frequencyData.length;
+		const barWidth = xSpread * 0.3;
+		const maxHeight = canvas.height * 0.25;
+		let xStart = 0, xEnd = 0;
 
 		frequencyData.forEach((node, index) => {
+			const nextNode = index === frequencyData.length - 1 ? node : frequencyData[index + 1];
 			const color = colorGenerator(node);
-			renderBars(barWidth, maxHeight, node, index, color);
+			xEnd = index * ( 1 + xSpread );
+
+			renderBars(xSpread, barWidth, maxHeight, node, index, color);
+			renderLine(xStart, xEnd, maxHeight, node, nextNode, color);
+
+			xStart = xEnd;
 		});
 	}
 	renderMirror();
